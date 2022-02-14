3119c40583dd3760bfcaf921ea17aa9690865aa9
Mayur-Mistry <mayurmistry022@gmail.com>
2022-02-14 18:29:42 +0530

    Change Project

---
+++ GuniApp.Web/Models/MyIdentityRole.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GuniApp.Web.Models
+{
+    public class MyIdentityRole
+      : IdentityRole<Guid>
+    {
+
+        [StringLength(100)]
+        public string Description { get; set; }
+
+    }
+}
---
+++ GuniApp.Web/Models/MyIdentityUser.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using GuniApp.Web.Models;
+
+
+namespace GuniApp.Web.Models
+{
+    public class MyIdentityUser
+    : IdentityUser<Guid>
+    {
+
+        [Display(Name = "Display Name")]
+        [Required]
+        [MinLength(3)]
+        [StringLength(60)]
+        public string DisplayName { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [PersonalData]                                  // for GDPR Compliance.
+        [Required]
+        [Column(TypeName = "smalldatetime")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Display(Name = "Is Admin User?")]
+        [Required]
+        public bool IsAdminUser { get; set; }
+
+    }
+
+}
